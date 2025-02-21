4f4d27d3f46733a5777aa64d1ddb50f2a6b1b614
Limeng Xie <43829518+lxie88@users.noreply.github.com>
2025-02-20 16:16:06 -0800

    Update kallisto_commonbean.sh

--- kallisto_commonbean.sh
+++ kallisto_commonbean.sh
@@ -1,59 +1,54 @@
 #!/bin/bash
-#SBATCH --job-name=Finalproject_momo# Job name
-#SBATCH --partition=batch # Partition (queue) name
-#SBATCH --ntasks=4 # Single task job
-#SBATCH --cpus-per-task=4 # Number of cores per task
-#SBATCH --mem=16gb # Total memory for job
-#SBATCH --time=20:00:00 # Time limit hrs:min:sec
-#SBATCH --output=/work/gene8940/lx83659/FinalProject/log.%j # Standard output and error log
-#SBATCH --mail-user=lx83659@uga.edu # Where to send mail
-#SBATCH --mail-type=END,FAIL # Mail events (BEGIN, END, FAIL, ALL)
+#SBATCH --job-name=RNAseq_Project        # Job name
+#SBATCH --partition=batch                # Partition (queue) name
+#SBATCH --ntasks=4                       # Number of tasks
+#SBATCH --cpus-per-task=4               # Number of cores per task
+#SBATCH --mem=16gb                       # Total memory for job
+#SBATCH --time=20:00:00                  # Time limit hrs:min:sec
+#SBATCH --output=/path/to/log.%j         # Standard output and error log
+# #SBATCH --mail-user=your_email@domain.com  # Uncomment and set if you want email notifications
+# #SBATCH --mail-type=END,FAIL               # Mail events (BEGIN, END, FAIL, ALL)
 
-# Download necessary modules 
-
+# Load necessary modules
 module load kallisto/0.43.1-foss-2016b
 module load R/3.4.4-foss-2016b-X11-20160819-GACRC
 module load SRA-Toolkit/2.9.1-centos_linux64
 
-# Downloads rna-seq reads using fastq-dump (SRA-Toolkit/2.9.1-centos_linux64)
-# You need to figure out which sequence you would like to download
-# download RNAseq data from https://www. ncbi.nlm.nih.gov/sra/PRJNA498535
-
-for n in {10..21};
-do  fastq-dump --split-files --gzip SRR81132${n}
+# Download RNA-seq data with fastq-dump (SRA Toolkit)
+# Data source: NCBI BioProject PRJNA498535
+for n in {10..21}; do
+  fastq-dump --split-files --gzip SRR81132${n}
 done
 
-# download Phaselous vulgaris cds sequence from Phytozome v10 https://genome.jgi.doe.gov/portal/pages/dynamicOrganismDownload.jsf?organism=Pvulgaris
+# Download Phaseolus vulgaris CDS from the JGI Phytozome portal
+# Make sure you have valid credentials to log in
+curl 'https://signon.jgi.doe.gov/signon/create' \
+  --data-urlencode 'login=YourLoginName' \
+  --data-urlencode 'password=YourPassword' \
+  -c cookies > /dev/null
 
-# login to the Phytozome website 
-curl 'https://signon.jgi.doe.gov/signon/create' --data-urlencode 'login=YourLoginame' --data-urlencode 'password=YourPassword' -c cookies > /dev/null
+curl "https://genome.jgi.doe.gov/portal/ext-api/downloads/get_tape_file?blocking=true&url=/Pvulgaris/download/_JAMO/57fecb5f7ded5e3135bc3576/Pvulgaris_442_v2.1.cds.fa.gz" \
+  -b cookies > Pvulgaris_cds.fa.gz
 
-# download cds sequence 
-curl "https://genome.jgi.doe.gov/portal/ext-api/downloads/get_tape_file?blocking=true&url=/Pvulgaris/download/_JAMO/57fecb5f7ded5e3135bc3576/Pvulgaris_442_v2.1.cds.fa.gz" -b cookies > Pvulgaris_cds.fa.gz
-gunzip Pvulgaris_cds.fa.gz 
+gunzip Pvulgaris_cds.fa.gz
 
-# Makes a kallisto index for cds sequence 
-kallisto index -i Pvulgaris_cds.index Pvulgaris_cds.fa    
+# Build Kallisto index
+kallisto index -i Pvulgaris_cds.index Pvulgaris_cds.fa
 
-# use kallisto quantify the reads 
+# Quantify reads using Kallisto
 mkdir output
-for i in {10..21};
-do 
-	dir="kallisto_SRR81132${i}" 
-	file1="SRR81132${i}_1.fastq.gz"
-        file2="SRR81132${i}_2.fastq.gz"
-  	kallisto quant -i Pvulgaris_cds.index -o $dir $file1 $file2 -b 100 -t 4
+
+for i in {10..21}; do
+  dir="kallisto_SRR81132${i}"
+  file1="SRR81132${i}_1.fastq.gz"
+  file2="SRR81132${i}_2.fastq.gz"
+  kallisto quant -i Pvulgaris_cds.index -o "$dir" "$file1" "$file2" -b 100 -t 4
 done
 
 mv kallisto_SRR81132* output/
 
-# create a directory to store slueth results 
-mkdir results 
+# Create a directory for Sleuth results
+mkdir results
 
-# start running slueth R commonbean script  
-
-Rscript sleuth_commonbean.R
-
-
-
-
+# Run the Sleuth R script
+Rscript sleuth.R
